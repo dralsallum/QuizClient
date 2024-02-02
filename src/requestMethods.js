e836a57976a1af4a4257a4b179dc9a374d9a4095
Saud Alsaloum <saudalsaloum@Sauds-MacBook-Pro.local>
2024-02-02 20:05:02 +0300

    fjadk

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://quizeng-022517ad949b.herokuapp.com/api";
+const BASE_URL = "https://quizeng-022517ad949b.herokuapp.com/api";
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
