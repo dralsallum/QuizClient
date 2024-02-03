98ae57bdf2a89a6dc313cf2d423c371c1b8e1fcf
Saud Alsaloum <saudalsaloum@Sauds-MacBook-Pro.local>
2024-02-03 18:20:03 +0300

    fklj

--- src/redux/LessonContext.js
+++ src/redux/LessonContext.js
@@ -1,52 +1,35 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { userRequest } from "../requestMethods";
+import React, { createContext, useContext, useState } from "react";
 
 const LessonContext = createContext();
 
-export const useLesson = () => useContext(LessonContext);
+export const useLesson = () => {
+  return useContext(LessonContext);
+};
 
 export const LessonProvider = ({ children }) => {
-  const [lessonsCompleted, setLessonsCompleted] = useState({});
-  const currentUser = useSelector((state) => state.user.currentUser);
+  const [lessonsCompleted, setLessonsCompleted] = useState({
+    1: [true, false, false, false, false],
+    2: [false, false, false, false, false],
+  });
 
-  useEffect(() => {
-    if (currentUser) {
-      fetchUserProgress(currentUser._id);
+  const incrementLesson = (chapterNumber) => {
+    let chapterLessons = lessonsCompleted[chapterNumber];
+    if (!chapterLessons) {
+      chapterLessons = [false, false, false, false, false];
     }
-  }, [currentUser]);
 
-  const fetchUserProgress = async (userId) => {
-    try {
-      const res = await userRequest.get(`/lessons/user/${userId}`);
-      setLessonsCompleted(res.data.lessonsCompleted);
-    } catch (err) {
-      console.error("Failed to fetch user progress:", err);
-    }
-  };
-
-  const incrementLesson = async (chapterNumber) => {
-    const chapterLessons =
-      lessonsCompleted[chapterNumber] || Array(5).fill(false);
-    const lessonToUpdate = chapterLessons.findIndex((completed) => !completed);
-
-    if (lessonToUpdate !== -1) {
-      chapterLessons[lessonToUpdate] = true;
-      setLessonsCompleted({
-        ...lessonsCompleted,
-        [chapterNumber]: chapterLessons,
-      });
-
-      try {
-        await userRequest.post("/lessons/complete", {
-          userId: currentUser._id,
-          chapterNumber,
-          lessonNumber: lessonToUpdate,
-        });
-      } catch (error) {
-        console.error("Error updating lesson completion status:", error);
+    for (let i = 0; i < chapterLessons.length; i++) {
+      if (!chapterLessons[i]) {
+        chapterLessons[i] = true;
+        break;
       }
     }
+
+    // Create a new object to force re-render
+    setLessonsCompleted({
+      ...lessonsCompleted,
+      [chapterNumber]: [...chapterLessons],
+    });
   };
 
   return (
