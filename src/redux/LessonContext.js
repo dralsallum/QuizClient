80865112ad03213ccfb6e524771fd431e6eb8fe7
Saud Alsaloum <saudalsaloum@Sauds-MacBook-Pro.local>
2024-02-03 12:38:30 +0300

    kldsfja

--- src/redux/LessonContext.js
+++ src/redux/LessonContext.js
@@ -1,22 +1,29 @@
-import React, { createContext, useContext, useState } from "react";
-import { useSelector } from "react-redux";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 const LessonContext = createContext();
 
-export const useLesson = () => {
-  return useContext(LessonContext);
-};
+export const useLesson = () => useContext(LessonContext);
 
 export const LessonProvider = ({ children }) => {
-  const [lessonsCompleted, setLessonsCompleted] = useState({
-    1: [true, false, false, false, false],
-    2: [false, false, false, false, false],
+  // Initialize state from localStorage if available
+  const [lessonsCompleted, setLessonsCompleted] = useState(() => {
+    const localData = localStorage.getItem("lessonsCompleted");
+    return localData
+      ? JSON.parse(localData)
+      : {
+          1: [true, false, false, false, false],
+          2: [false, false, false, false, false],
+        };
   });
+
+  useEffect(() => {
+    // Save to localStorage when lessonsCompleted changes
+    localStorage.setItem("lessonsCompleted", JSON.stringify(lessonsCompleted));
+  }, [lessonsCompleted]);
 
   const incrementLesson = (chapterNumber) => {
     let chapterLessons = lessonsCompleted[chapterNumber];
     if (!chapterLessons) {
-      // Initialize
       chapterLessons = [false, false, false, false, false];
     }
 
@@ -27,10 +34,14 @@ export const LessonProvider = ({ children }) => {
       }
     }
 
-    // Create a new object to force re-render
-    setLessonsCompleted({
-      ...lessonsCompleted,
-      [chapterNumber]: [...chapterLessons],
+    // Update state and localStorage
+    setLessonsCompleted((prevLessonsCompleted) => {
+      const updatedLessons = {
+        ...prevLessonsCompleted,
+        [chapterNumber]: [...chapterLessons],
+      };
+      localStorage.setItem("lessonsCompleted", JSON.stringify(updatedLessons));
+      return updatedLessons;
     });
   };
 
