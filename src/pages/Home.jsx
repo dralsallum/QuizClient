1fb7405c3fe0f32f1f641f09b833567a7f07045e
Saud Alsaloum <saudalsaloum@Sauds-MacBook-Pro.local>
2024-05-16 14:19:09 +0300

    fjkslad

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
-import { NavbarQui, Body, NavAud, FooterMe } from "../components";
+import { Body, NavAud, FooterMe } from "../components";
 
 const Home = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
       <NavAud />
