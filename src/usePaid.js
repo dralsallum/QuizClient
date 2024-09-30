4b690878331a1aac8591dd7762ffaa790916d517
Saud Alsaloum <saudalsaloum@Sauds-MacBook-Pro.local>
2024-09-30 08:58:21 +0300

    fljds

--- src/usePaid.js
+++ src/usePaid.js
@@ -1,8 +1,6 @@
-// src/components/ProtectedRoute.js
 import React from "react";
 import { Navigate, useParams, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
-import GoogleAd from "../src/components/Google/Google"; // Import the GoogleAd component
 
 export const ProtectedRoute = ({ children }) => {
   const user = useSelector((state) => state.user.currentUser);
@@ -17,21 +15,28 @@ export const ProtectedRoute = ({ children }) => {
     return <Navigate to="/signup" />;
   }
 
-  // Determine if the current route requires payment
-  const isProtectedRoute =
-    (location.pathname.startsWith("/vocabulary/") &&
-      !user.isPaid &&
-      !freeAccessVocabSets.includes(vocabSet)) ||
-    (location.pathname.startsWith("/audio/listen/") &&
-      !user.isPaid &&
-      !freeAccessStoryUrls.includes(storyUrl)) ||
-    (location.pathname.startsWith("/test/") &&
-      !user.isPaid &&
-      !freeAccessChapters.includes(chapterName));
+  if (
+    location.pathname.startsWith("/vocabulary/") &&
+    !user.isPaid &&
+    !freeAccessVocabSets.includes(vocabSet)
+  ) {
+    return <Navigate to="/cashout" />;
+  }
 
-  if (isProtectedRoute) {
-    // Instead of navigating to /cashout, display Google Ads
-    return <GoogleAd />;
+  if (
+    location.pathname.startsWith("/audio/listen/") &&
+    !user.isPaid &&
+    !freeAccessStoryUrls.includes(storyUrl)
+  ) {
+    return <Navigate to="/cashout" />;
+  }
+
+  if (
+    location.pathname.startsWith("/test/") &&
+    !user.isPaid &&
+    !freeAccessChapters.includes(chapterName)
+  ) {
+    return <Navigate to="/cashout" />;
   }
 
   return children;
